1d19267fdb6aa4ada2bd4293cf43c710d976d43c
gustavopxavier <gustavopxavier@gmail.com>
2021-03-16 14:39:39 -0300

    Update to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,11 +5,12 @@ class Pessoa {
     var nome: String = "Gustavo"
     var cpf: String = "123.123.123-12"
     private set
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
     val gustavo = Pessoa()
 
-    println(gustavo.nome)
-    println(gustavo.cpf)
+    println(gustavo.pessoaInfo())
 }
